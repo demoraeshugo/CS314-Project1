712310bc603b4c7e4a8194043fdef8cd2ceb4c1f
Hugo De Moraes <demoraeshugo@gmail.com>
2020-03-22 22:38:14 -0400

    Optimizer changes

--- Optimizer.c
+++ Optimizer.c
@@ -123,7 +123,7 @@ int main()
 	return EXIT_SUCCESS;
 }
 */
-
+/*
 int main()
  {
  	Instruction *head;
@@ -132,8 +132,6 @@ int main()
  		ERROR("No instructions\n");
  		exit(EXIT_FAILURE);
  	}
-
-	/* YOUR CODE GOES HERE */
 
  	Instruction *first;
  	Instruction *second;
@@ -191,4 +189,91 @@ int main()
  	PrintInstructionList(stdout, head);
  	DestroyInstructionList(head);
  	return EXIT_SUCCESS;
+}
+*/
+
+int main()
+{
+	Instruction *head;
+	head = ReadInstructionList(stdin);
+	if (!head)
+	{
+		WARNING("No instructions\n");
+		exit(EXIT_FAILURE);
+	}
+	/* YOUR CODE GOES HERE */
+	Instruction *first;
+	Instruction *second;
+	Instruction *third;
+
+	Instruction *instr = head;
+
+	bool con1;
+	bool con2;
+	bool con3;
+	bool con4;
+
+	do
+	{
+		first = instr;
+		second = instr->next;
+		third = second->next;
+		if (first->opcode == LOADI && second->opcode == LOADI)
+		{
+			con1 = first->field1 == third->field2;
+			con2 = second->field1 == third->field3;
+			con3 = first->field1 == third->field3;
+			con4 = second->field1 == third->field2;
+			switch (third->opcode)
+			{
+			case ADD:
+				if ((con1 && con2) || (con3 && con4))
+				{
+					instr->field1 = third->field1;
+					instr->field2 = first->field2 + second->field2;
+					instr->next = third->next;
+					third->next->prev = instr;
+					free(second);
+					free(third);
+					printf("Found ADD optimization.\n");
+				}
+				break;
+			case MUL:
+				if ((con1 && con2) || (con3 && con4))
+				{
+					instr->field1 = third->field1;
+					instr->field2 = first->field2 * second->field2;
+					instr->next = third->next;
+					third->next->prev = instr;
+					free(second);
+					free(third);
+					printf("Found MUL optimization.\n");
+				}
+				break;
+			case SUB:
+				if (con1 && con2)
+				{
+					instr->field1 = third->field1;
+					instr->field2 = first->field2 - second->field2;
+					instr->next = third->next;
+					third->next->prev = instr;
+					free(second);
+					free(third);
+					printf("Found SUB optimization.\n");
+				}
+				break;
+			default:
+				printf("\nNot optimizable.\n");
+				break;
+			}
+		}
+		instr = instr->next;
+	} while (instr != NULL && instr->next != NULL && instr->next->next != NULL);
+
+	if (head)
+	{
+		PrintInstructionList(stdout, head);
+		DestroyInstructionList(head);
+	}
+	return EXIT_SUCCESS;
 }
